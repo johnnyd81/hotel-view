acef10cb6b62c0ba19cd6ae9bf6a4b59d747a356
Jonathan Johnson <jonathandjohnson81@gmail.com>
2024-11-14 14:20:13 +0200

    added a new component

---
+++ src/components/resultlistmodal/ResultListModal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./resultListModal.css";
+
+const ResultListModal = () => {
+  return <div className="resultListModal"></div>;
+};
+
+export default ResultListModal;
