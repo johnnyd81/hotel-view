11685be5e6c6ed07e6ab999664bc0a10c8ee891a
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-11-30 16:23:34 +0200

    Update Main.jsx

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -4,7 +4,7 @@ import TripPlanner from "../../components/tripPlanner/TripPlanner";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 
-// main page component i.e. the homepage
+// main homepage component
 const Main = () => {
   return (
     <div>
