b66ee3ddbdf06a5c871dae21ac483ea23d19e5f3
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-12-05 20:25:49 +0200

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault(); //prevent the default form actions i.e. refreshing the webpage
 
+    // call the login function with the username and password
     await login(username, password);
   };
 
