5d3001ddcfa251e21765a65bda81c682e9eaf206
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-12-11 13:10:26 +0200

    Update BookingContext.jsx

--- src/context/BookingContext.jsx
+++ src/context/BookingContext.jsx
@@ -19,6 +19,8 @@ export const bookingReducer = (state, action) => {
   }
 };
 
+//the provider function takes the entire app as a child
+//the value prop is therefore available globally to the entire app
 export const BookingContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(bookingReducer, {
     bookings: [],
