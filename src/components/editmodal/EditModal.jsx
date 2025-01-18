7e442a194a5bd0e644ee8334073e417a1b710db6
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-01-18 23:52:01 +0200

    Update EditModal.jsx

--- src/components/editmodal/EditModal.jsx
+++ src/components/editmodal/EditModal.jsx
@@ -15,7 +15,7 @@ const EditModal = ({ user, setOpen }) => {
   const { server } = useAuthContext();
 
   const editUser = async (id) => {
-    //newName stores the updated name for a user and only a admin can perform this action
+    //newName stores the updated name for a user and only a admin can update a username
     const newName = { username };
 
     const response = await fetch(server + "/api/users/" + id, {
