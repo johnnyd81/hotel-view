db9342f3548f996b2dc704f300e5340e4e9edec8
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-03-21 16:30:29 +0200

    Update DestItem.jsx

--- src/components/destItem/DestItem.jsx
+++ src/components/destItem/DestItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./destItem.css";
 
 //destination item component to display the various travel destinations
-// the item prop contains all the information the DestItem component needs i.e. the picture, id, title etc.
+// the item prop object contains all the information the DestItem component needs i.e. the picture, id, title etc.
 const DestItem = ({ item }) => {
   return (
     <div className="dest-item" key={item.id}>
