42f82c7ff11e0c28c9a81909ecfaf6ac8122c001
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-05-16 15:07:02 +0200

    Update SearchContext.jsx

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useReducer } from "react";
 
-//allows the search values to be available in the entire app by making it global
+//allows the search values to be available in the entire app by making it a global variable
 export const searchContext = createContext();
 
 //The searchReducer function modifies the global search state object i.e. destination, date and choices
